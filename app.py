5d0e2a97fb13ae5353cd5373b5ce7a9043e47fc7
Scarlett Alejandra Sotelo Serrano <scarlett@Scarletts-MacBook-Pro.local>
2022-01-26 21:55:00 -0600

    Add mysql test connection

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, make_response, redirect, render_template
 from datetime import datetime
+from flask_mysqldb import MySQL
 
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'pandachrondb'
+mysql = MySQL(app)
 
 if __name__ == "__main__":
-    app.run(port = 3000, debug = True)
+    app.run(port=3000, debug=True)
 
 
 todos = ['TODO 1', 'TODO 2', 'TODO 3']
@@ -13,12 +19,13 @@ todos = ['TODO 1', 'TODO 2', 'TODO 3']
 
 @app.route('/')
 def index():
-    return 'index'
+    current_year = datetime.now().year
+    return render_template('base.html', current_year=current_year)
+
 
 @app.route('/add_task')
 def add_task():
     context = {
-            'current_year': datetime.now().year
+        'current_year': datetime.now().year
     }
-    return render_template('index.html', **context)
-
+    return render_template('index.html', context)
